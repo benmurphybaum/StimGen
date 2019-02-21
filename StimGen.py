879c0fecd8d7d0f71ac03f14f4c7ad56a74aa8bc
Ben Murphy-Baum <bmurphy.baum@gmail.com>
2019-02-21 11:20:15 -0800

    testing

--- StimGen.py
+++ StimGen.py
@@ -1,1 +1,11 @@
 #StimGen python3
+
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QVBoxLayout
+StimGen = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QApplication('Bottom'))
+window.setLayout(layout)
+window.show()
+app.exec_()
